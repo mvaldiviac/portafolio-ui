63fe797199f5daaa458b6a500eb60d639462a8b2
mvaldiviac <m.valdivia.c@gmail.com>
2025-06-21 01:16:01 -0400

    actualizado con bloqueo de margenes eje x

--- js/styles.js
+++ js/styles.js
@@ -25,3 +25,18 @@ function changeStyle() {
     }
     
 }
+
+//USO DE MENU
+const menu = document.querySelector('.menu-mb');
+const lat = document.querySelector('#lat');
+const abrirHambur = document.querySelector('#clickHam');
+
+menu.addEventListener('click',()=>{
+  menu.classList.toggle("active");
+  lat.classList.toggle("cheked");
+  
+})
+lat.addEventListener('click',()=>{
+  lat.classList.toggle("cheked");
+  menu.classList.toggle("active");
+})
